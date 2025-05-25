7296030f4309cddc8e3badaac7d3b57831f7b89a
Joan Nyawira Kori <korijoan171@gmail.com>
2025-05-26 00:12:45 +0300

    Freebies management

--- lib/debug.py
+++ lib/debug.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 from sqlalchemy import create_engine
-from models import Base, Company, Dev, Freebie 
+from sqlalchemy.orm import sessionmaker
+from models import Base, Company, Dev, Freebie,engine
+
+Session = sessionmaker(bind=engine)
+session = Session()
 
 if __name__ == '__main__':
     engine = create_engine('sqlite:///lib/freebies.db')
