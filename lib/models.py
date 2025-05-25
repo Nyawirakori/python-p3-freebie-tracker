e80caa52fd476a98cca0d8882511b9ce284a3682
Joan Nyawira Kori <korijoan171@gmail.com>
2025-05-25 22:53:58 +0300

    Freebies management

--- lib/models.py
+++ lib/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy import ForeignKey, Column, Integer, String, MetaData, create_engine
+from sqlalchemy.orm import relationship, backref, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 convention = {
@@ -7,23 +7,80 @@ convention = {
 }
 metadata = MetaData(naming_convention=convention)
 
+engine = create_engine('sqlite:///freebies.db')
+
 Base = declarative_base(metadata=metadata)
 
+
+#Company table
 class Company(Base):
     __tablename__ = 'companies'
 
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     founding_year = Column(Integer())
+    
+    #relationship- backref automatically creates the reverse reference
+    freebies = relationship('Freebie', backref='company')
 
     def __repr__(self):
         return f'<Company {self.name}>'
+    
+    def give_freebie(self, dev, item_name, value):
+        new_freebie = Freebie(item_name=item_name, value=value, dev=dev, company=self)
+        session.add(new_freebie)
+        session.commit()
+        return new_freebie
+    
+    @classmethod
+    def oldest_company(cls):
+        return session.query(cls).order_by(cls.founding_year).first()
 
+
+
+#Devs table
 class Dev(Base):
     __tablename__ = 'devs'
 
     id = Column(Integer(), primary_key=True)
     name= Column(String())
 
+    #relationship
+    freebies = relationship('Freebie', backref='dev')
+
     def __repr__(self):
         return f'<Dev {self.name}>'
+    
+    def received_one(self, item_name):
+        return any(freebie.item_name == item_name for freebie in self.freebies)
+    
+    def give_away(self, dev, freebie):
+        if freebie in self.freebies:
+            freebie.dev = dev
+            session.commit()
+            return True
+        return False
+
+
+    
+#Freebies table
+class Freebie(Base):
+    __tablename__ = 'freebies'
+    id = Column(Integer(), primary_key=True)
+    item_name = Column(String())
+    value = Column(Integer())
+    dev_id = Column(Integer(), ForeignKey('devs.id'))
+    company_id = Column(Integer(), ForeignKey('companies.id'))
+
+    def __repr__(self):
+        return f'<Freebie {self.item_name}>'
+        
+    def print_details(self):
+        return f"{self.dev.name} owns a {self.item_name} from {self.company.name}."
+
+
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
+    
--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,51 @@
 #!/usr/bin/env python3
 
 # Script goes here!
+
+#!/usr/bin/env python3
+
+# Script goes here!
+from models import Dev, Company, Freebie, Base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+engine =create_engine('sqlite://freebies.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+Base.metadata.create_all(engine)
+
+# Clear out old data for a fresh start
+session.query(Freebie).delete()
+session.query(Dev).delete()
+session.query(Company).delete()
+session.commit()
+
+# Create companies
+c1 = Company(name="Highlands Drinks", founding_year=1954)
+c2 = Company(name="Jitu", founding_year=2010)
+c3 = Company(name="Safaricom", founding_year=1997)
+
+# Create devs
+d1 = Dev(name="Joan")
+d2 = Dev(name="Ella")
+d3 = Dev(name="Kori")
+
+# Add and commit to get IDs assigned
+session.add_all([c1, c2, c3, d1, d2, d3])
+session.commit()
+
+# Create freebies (each links dev and company)
+f1 = Freebie(item_name="Tote bag", value=20, dev_id=d1.id, company_id=c1.id)
+f2 = Freebie(item_name="Hoodies", value=15, dev_id=d2.id, company_id=c2.id)
+f3 = Freebie(item_name="T-shirts", value=21, dev_id=d1.id, company_id=c3.id)
+f4 = Freebie(item_name="Ear pods", value=9, dev_id=d3.id, company_id=c1.id)
+f5 = Freebie(item_name="Notebook", value=7, dev_id=d2.id, company_id=c3.id)
+f6 = Freebie(item_name="Pen", value=19, dev_id=d3.id, company_id=c2.id)
+
+session.add_all([f1, f2, f3, f4, f5, f6])
+session.commit()
+
+print("Database seeded!")
